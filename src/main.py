53e318ee54a70f981770d9875a1549132275ad4f
Brandon Liu <byliu88@gmail.com>
2021-05-10 23:03:15 +0800

    added requirements.txt and run.sh

--- src/main.py
+++ src/main.py
@@ -8,8 +8,6 @@ def screenshot(t):
     screenshot = pyautogui.screenshot()
     d1 = date.today().strftime("%d%m%Y")
     screenshot.save("../images/screenshot_" + d1 + ".png")
-    # pyautogui.hotkey('f5') #Simulates F5 key press = page refresh
-    # print("did it")
     return
 
 schedule.every().day.at("00:00").do(screenshot,'It is 00:00')
